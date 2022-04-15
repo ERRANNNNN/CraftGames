3eb0bae5815f0ac919bdbdedf5c51d06df78b7d4
ERRAN <errannnnnnn@gmail.com>
2022-04-15 12:43:18 +0800

    Booster.cs добавил бустер

---
+++ Assets/Scripts/Game/Boosters/Booster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Booster : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private TextMeshProUGUI boosterCountTextMesh;
+    public static Action OnBoosterUsed;
+    private int boosterCount;
+
+    public void Init(int _boosterCount)
+    {
+        boosterCount = _boosterCount;
+        boosterCountTextMesh.text = boosterCount.ToString();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (boosterCount != 0)
+        {
+            OnBoosterUsed?.Invoke();
+            boosterCount--;
+            boosterCountTextMesh.text = boosterCount.ToString();
+        }
+    }
+}
